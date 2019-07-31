d6b434a02401fdabd0f14c4eecb2921b74b8b20d
145732 <145732@shinsegae.com>
2019-07-31 09:24:47 +0900

    header 추가

---
+++ src/components/layouts/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="mcom_tit_renew ty_top">
+          <h2 className="mcom_tit_txt">장바구니</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Header;
