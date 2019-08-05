dff28f0f23de43f00e668001740b2c6a9f9bebaa
145732 <145732@shinsegae.com>
2019-08-05 16:13:55 +0900

    header 스타일 추가

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import style from './Header.module.scss';
 
 const Header = () => {
   return (
-    <div className="header">
-        <div className="mcom_tit_renew ty_top">
-          <h2 className="mcom_tit_txt">장바구니</h2>
+    <div className={style.header}>
+      <div className={`${style.mcom_tit_renew} ${style.ty_top}`}>
+        <h2 className={style.mcom_tit_txt}>장바구니</h2>
+        <div className={style.mcom_tit_lft}>
+          <a href="#" className={style.btn_back}><span className={`${style.sp_ctg_icon} ${style.ctg_icon_back}`}><span className="blind">이전 페이지</span></span></a>
         </div>
+        <div className={style.mcom_tit_rgt}>
+          <div className={style.btn_cate}>
+            <button type="button"><span className={`${style.sp_ctg_icon} ${style.ctg_icon_search}`}><span className="blind">검색</span></span></button>
+          </div>
+          <div className={style.btn_cate}>
+            <a href="#"><span className={`${style.sp_ctg_icon} ${style.ctg_icon_home}`}><span className="blind">SSG.com 홈</span></span></a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
