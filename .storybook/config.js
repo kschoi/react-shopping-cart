f0d53baaeef43720446522a06b2d7463dac84766
145732 <145732@shinsegae.com>
2019-08-06 13:03:58 +0900

    스토리북 폴더 변경

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,15 @@
 import { configure } from '@storybook/react';
 
+const req = require.context(
+  '../src/components',
+  true,
+  /\.stories\.(js|jsx)$/,
+)
+
 function loadStories() {
-    require('../stories/index.js');
-    // You can require as many stories as you need.
+  req.keys().forEach(filename => {
+    req(filename)
+  })
 }
 
 configure(loadStories, module);
