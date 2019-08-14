155d2ed1566fe123bf30fee22f6ae304b96b4fcd
145732 <145732@shinsegae.com>
2019-08-14 10:11:48 +0900

    stroybook typescript 추가

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,9 @@
 import { configure } from '@storybook/react';
 
-const req = require.context(
-  '../src/components',
-  true,
-  /\.stories\.(ts|tsx)$/,
-)
+const req = require.context('../src/components', true, /\.stories\.(js|tsx)$/);
 
 function loadStories() {
-  req.keys().forEach(filename => {
-    req(filename)
-  })
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
